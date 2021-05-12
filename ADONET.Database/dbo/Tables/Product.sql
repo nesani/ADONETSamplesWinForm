279a62ec391c54cd660001ccfa956d8d0a87fa99
Nenad Nikic <nenad.nikic@dti.rs>
2021-05-12 13:27:40 +0200

    imported database project

--- ADONET.Database/dbo/Tables/Product.sql
+++ ADONET.Database/dbo/Tables/Product.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Product] (
-    [id]         INT        primary key    IDENTITY (1, 1) NOT NULL,
+    [id]         INT        Primary Key    IDENTITY (1, 1) NOT NULL,
     [Name]       VARCHAR (20)   NULL,
     [Date]       DATETIME       NULL,
     [URL]        VARCHAR (30)   NULL,
@@ -7,3 +7,7 @@
     [RetireDate] DATETIME       NULL
 );
 
+
+GO
+
+CREATE INDEX [IX_Product_Name] ON [dbo].[Product] ([Name])
--- ADONET.Database/dbo/Tables/ProductCategory.sql
+++ ADONET.Database/dbo/Tables/ProductCategory.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[ProductCategory] (
-    [id]   INT     primary key       IDENTITY (1, 1) NOT NULL,
+    [id]   INT      Primary Key      IDENTITY (1, 1) NOT NULL,
     [Name] NVARCHAR (250) NULL
 );
 
